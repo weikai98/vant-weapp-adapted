5d264ddc06af57f72043cf4ef6c1d5ecf37d0b45
weikaikai <1394987421@qq.com>
2022-05-27 11:29:50 +0800

    ✨ 新增: transition组件

--- example/pages/transition/index.js
+++ example/pages/transition/index.js
@@ -2,5 +2,37 @@ import Page from '../../common/page';
 
 Page({
   data: {
+    name: '',
+    show: false
   },
+  openTransition(e) {
+    const name = e.currentTarget.dataset.item
+    this.setData({
+      name,
+      show: true
+    })
+    setTimeout(() => {
+      this.setData({
+        show: false
+      })
+     }, 1000)
+  },
+  beforeEnter() {
+    console.log('beforeEnter')
+  },
+  enter() {
+    console.log('enter')
+  },
+  afterEnter() {
+    console.log('afterEnter')
+  },
+  beforeLeave() {
+    console.log('beforeLeave')
+  },
+  leave() {
+    console.log('leave')
+  },
+  afterLeave() {
+    console.log('afterLeave')
+  }
 });
--- packages/transition/index.ts
+++ packages/transition/index.ts
@@ -1,3 +1,105 @@
-import { VantComponent } from "../common/components";
+import { requestAnimationFrame } from "../common/utils"
+import { VantComponent } from "../common/components"
 
-VantComponent({})
+const autoCssTransition = (name: string) => ({
+  enter: `enter ${name}-enter`,
+  "enter-active": `enter-active ${name}-enter-active`,
+  "enter-to": `enter-to ${name}-enter-to`,
+  leave: `leave ${name}-leave`,
+  "leave-active": `leave-active ${name}-leave-active`,
+  "leave-to": `leave-to ${name}-leave-to`,
+})
+
+VantComponent({
+  classes: [
+    "enter-class",
+    "enter-active-class",
+    "enter-to-class",
+    "leave-class",
+    "leave-active-class",
+    "leave-to-class",
+  ],
+  props: {
+    show: {
+      type: Boolean,
+      value: true,
+      observer: "oberverShow",
+    },
+    name: {
+      type: String,
+      value: "fade",
+    },
+    duration: {
+      type: Number,
+      value: 1000,
+    },
+  },
+  data: {
+    visible: false,
+    classes: "",
+  },
+  mounted() {
+    if (this.data.show) {
+      this.oberverShow(true)
+    }
+  },
+  methods: {
+    oberverShow(v: boolean, oldv?: boolean) {
+      if (v === oldv) return
+      if (v) {
+        this.enter()
+      } else {
+        this.leave()
+      }
+    },
+    enter() {
+      const { name } = this.data
+      const classNames = autoCssTransition(name)
+      this.setData({
+        visible: true,
+        classes: classNames["enter"],
+      })
+      this.$emit("beforeEnter")
+      requestAnimationFrame(() => {
+        this.setData({
+          classes: classNames["enter-active"],
+        })
+        this.$emit("enter")
+
+        requestAnimationFrame(() => {
+          this.setData({
+            classes: classNames["enter-to"],
+          })
+          this.$emit("afterEnter")
+        })
+      })
+    },
+    leave() {
+      const { name, duration } = this.data
+      const classNames = autoCssTransition(name)
+      this.$emit("beforeLeave")
+      this.setData({
+        classes: classNames["leave"],
+      })
+
+      requestAnimationFrame(() => {
+        this.$emit("leave")
+        this.setData({
+          classes: classNames["leave-active"],
+        })
+
+        requestAnimationFrame(() => {
+          setTimeout(() => {
+            this.setData({
+              visible: false,
+            })
+            this.$emit("afterLeave")
+          }, duration)
+          this.setData({
+            classes: classNames["leave-to"],
+          })
+        })
+      })
+    },
+  },
+})
